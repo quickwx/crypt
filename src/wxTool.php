c27ab217a33eb3d35e2517763acb44dd7747f755
刘磊 <369968620@qq.com>
2021-02-02 11:26:52 +0800

    test

--- src/wxTool.php
+++ src/wxTool.php
@@ -5,11 +5,13 @@
  * @Author liulei email liulei@369968620.com
  * Date 2021/2/2 10:56 上午
  */
+namespace quickwx;
 
 class wxTool
 {
     public function decryptMsg ($msgSignature, $timestamp = null, $nonce, $postData, &$msg)
     {
         $msg = 1;
+        return $msg;
     }
 }
