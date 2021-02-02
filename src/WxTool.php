1cd20eb23cadaee2072daa20ef71beaedf079a70
刘磊 <369968620@qq.com>
2021-02-02 19:23:25 +0800

    DEBUG

--- src/WxTool.php
+++ src/WxTool.php
@@ -19,7 +19,7 @@ class WxTool
 
     private $appId;
 
-    public function __construct($appId,$token, $encodingAesKey)
+    public function __construct($appId,$token,$encodingAesKey)
     {
         $this->token = $token;
         $this->encodingAesKey = $encodingAesKey;
