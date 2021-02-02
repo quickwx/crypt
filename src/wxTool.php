6bc4a37f327a61fe003be4b2cbc37bb47484464c
刘磊 <369968620@qq.com>
2021-02-02 10:58:58 +0800

    test

---
+++ src/wxTool.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by Crypt
+ * @Desc
+ * @Author liulei email liulei@369968620.com
+ * Date 2021/2/2 10:56 上午
+ */
+
+class wxTool
+{
+    public function decryptMsg ($msgSignature, $timestamp = null, $nonce, $postData, &$msg)
+    {
+        $msg = 1;
+    }
+}
